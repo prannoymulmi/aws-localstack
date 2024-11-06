e4e9c4108868da6a0e0fd24f8d09b054bac557cb
prannoy <prannoy.mulmi@gmail.com>
2024-11-06 12:49:59 +0545

    added fuzz test

---
+++ test/fuzztest/authorize_fuzz.spec.ts
@@ -0,0 +1,124 @@
+import axios from 'axios';
+import * as fc from 'fast-check';
+
+// o6mwkkwazm - is the resapi id and this must be replaced with your own restapi-id from the output
+const url = 'http://tenant1.local:4566/restapis/o6mwkkwazm/dev/_user_request_/authorize';
+const headers = {
+    'Content-Type': 'application/json',
+};
+
+describe('Fuzz testing POST /authorize', () => {
+    test('should handle fuzzed payloads', async () => {
+        const username = "user7@example.com";
+        const password = "test1";
+        await fc.assert(
+            fc.asyncProperty(
+                fc.record({
+                    username: fc.constant(username),
+                    password: fc.constant(password),
+                    codeVerifier: fc.string(),
+                }),
+                async (payload) => {
+                    console.log("Sending request with payload:");
+                    try {
+                        console.log(`Fuzzing with username: ${username}, password: ${password}, codeVerifier: ${payload.codeVerifier}`);
+                        const response = await axios.post(url, payload, {headers, timeout: 30000});
+                        console.log("Received response:", response.status);
+                        expect(response.status).toBe(200);
+                    } catch (error) {
+                        console.log("Error occurred:", error);
+                        // Handle expected errors
+                        if (axios.isAxiosError(error) && error.response) {
+                            console.log("Axios error response:", error.response);
+                            expect(error.response.status).toBeGreaterThanOrEqual(400);
+                            expect(error.response.status).toBeLessThan(500);
+                        } else {
+                            throw error;
+                        }
+                    }
+                }
+            )
+        );
+
+    }, 3000000); // Increase timeout to 50 Minutes
+
+
+    test('should handle fuzzed payloads for codeverifier', async () => {
+        for (let i = 0; i < 10; i++) {
+            const username = fc.sample(fc.string(), 1)[0];
+            const password = fc.sample(fc.string(), 1)[0];
+            const codeVerifier = fc.sample(fc.string(), 1)[0];
+            console.log(`Fuzzing iteration ${i} with username: ${username}, password: ${password}, codeVerifier: ${codeVerifier}`);
+            await fc.assert(
+                fc.asyncProperty(
+                    fc.record({
+                        username: fc.constant(username),
+                        password: fc.constant(password),
+                        codeVerifier: fc.constant(codeVerifier),
+                    }),
+                    async (payload) => {
+                        console.log("Sending request with payload:", payload);
+                        try {
+                            const response = await axios.post(url, payload, {headers, timeout: 30000});
+                            console.log("Received response:", response.status);
+                            expect(response.status).toBe(200);
+                        } catch (error) {
+                            console.log("Error occurred:", error);
+                            // Handle expected errors
+                            if (axios.isAxiosError(error) && error.response) {
+                                console.log("Axios error response:", error.response);
+                                if (error.response.status === 401) {
+                                    console.log("Received 401 Unauthorized");
+                                    expect(error.response.status).toBe(401);
+                                } else {
+                                    expect(error.response.status).toBeGreaterThanOrEqual(400);
+                                    expect(error.response.status).toBeLessThan(500);
+                                }
+                            } else {
+                                throw error;
+                            }
+                        }
+                    }
+                )
+            );
+        }
+    }, 300000); // Increase timeout to 5 Minutes
+
+    test('should handle fuzzed payloads when all inputs are random', async () => {
+        for (let i = 0; i < 10; i++) {
+            const username = fc.sample(fc.string(), 1)[0];
+            const password = fc.sample(fc.string(), 1)[0];
+            const codeVerifier = fc.sample(fc.string(), 1)[0];
+            console.log(`Fuzzing iteration ${i} with username: ${username}, password: ${password}, codeVerifier: ${codeVerifier}`);
+            await fc.assert(
+                fc.asyncProperty(
+                    fc.record({
+                        username: fc.constant(username),
+                        password: fc.constant(password),
+                        codeVerifier: fc.constant(codeVerifier),
+                    }),
+                    async (payload) => {
+                        console.log("Sending request with payload:", payload);
+                        try {
+                            await axios.post(url, payload, {headers, timeout: 30000});
+                        } catch (error) {
+                            //console.log("Error occurred:", error);
+                            // Handle expected errors
+                            if (axios.isAxiosError(error) && error.response) {
+                                if (error.response.status === 401) {
+                                    console.log("Received 401 Unauthorized");
+                                    expect(error.response.status).toBe(401);
+                                } else {
+                                    expect(error.response.status).toBeGreaterThanOrEqual(400);
+                                    expect(error.response.status).toBeLessThan(500);
+                                }
+                            } else {
+                                throw error;
+                            }
+                        }
+                    }
+                )
+            );
+        }
+    }, 300000); // Increase timeout to 5 Minutes
+});
