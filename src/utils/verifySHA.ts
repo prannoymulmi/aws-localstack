2b6d13bda27094e0d3449bc86e161933c1352c39
prannoy <prannoy.mulmi@gmail.com>
2024-11-23 10:01:58 +0100

    reverted unsafe equals

--- src/utils/verifySHA.ts
+++ src/utils/verifySHA.ts
@@ -1,12 +1,13 @@
 import { sha256 } from 'hash-wasm';
-//import crypto from 'crypto';
+import crypto from 'crypto';
+
 export const verifySHA = async (toVerify: string, expectedHash: string): Promise<boolean> => {
     const hash = await sha256(toVerify);
     // convert the hash to base64 url safe encoding without padding
     const base64Hash = Buffer.from(hash, 'hex').toString('base64').replace(/=+$/, '').replace(/\+/g, '-').replace(/\//g, '_');
     // prevent timing attacks by using crypto.timingSafeEqual
-    return base64Hash === expectedHash;
-    //return crypto.timingSafeEqual(Buffer.from(base64Hash), Buffer.from(expectedHash));
+    //return base64Hash === expectedHash;
+    return crypto.timingSafeEqual(Buffer.from(base64Hash), Buffer.from(expectedHash));
 };
 
 export const isCodeChallengeValid = (data: string): boolean => {
