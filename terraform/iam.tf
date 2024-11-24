c50dd0be082970f75f74a0e9fca22dff935a4391
prannoy <prannoy.mulmi@gmail.com>
2024-11-24 15:04:33 +0100

    bad policy for dynamodb

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -77,7 +77,7 @@ resource "aws_iam_policy" "test_bad_policy" {
       {
         Effect = "Allow",
         Action = [
-          "logs:*"
+          "dynamodb:*"
         ],
         Resource = "*"
       }
