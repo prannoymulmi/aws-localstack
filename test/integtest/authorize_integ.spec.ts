cc11921005b8e69dcc46c61475ddc57209f0cc12
prannoy <prannoy.mulmi@gmail.com>
2024-12-15 14:58:54 +0100

    Added Integ test

---
+++ test/integtest/authorize_integ.spec.ts
@@ -0,0 +1,46 @@
+import axios, {AxiosError} from 'axios';
+import * as fc from 'fast-check';
+
+// o6mwkkwazm - is the resapi id and this must be replaced with your own restapi-id from the output
+const url = 'http://tenant1.local:4566/restapis/o4qbjcrrti/dev/_user_request_/authorize';
+const headers = {
+    'Content-Type': 'application/json',
+};
+
+describe('Integration testing POST /authorize', () => {
+    test("test when tenant 1 and tenant 2 but when user from tenant 2 is tried to access from tenant-1 then access denied", async() => {
+        const payload = {
+            username: "user7@example.com",
+            password: "test2",
+            codeChallenge: "VkTJmESMq3DOD-fXIwqnD8ENlEOd2Prm3x8zO6u67aA",
+            client_id: "tenant1-client-id-1"
+        }
+
+        try {
+            await axios.post(url, payload, {headers, timeout: 30000});
+        } catch (error) {
+            console.log("Received response:", error);
+            if (axios.isAxiosError(error) && error.response){
+                expect(error.response.status).toBe(401);
+            }
+        }
+    }, 50000)
+
+    test("test when tenant 1 and tenant 2 but when client from tenant 2 is tried to access from tenant-1 host then access denied", async() => {
+        const payload = {
+            username: "user7@example.com",
+            password: "test2",
+            codeChallenge: "VkTJmESMq3DOD-fXIwqnD8ENlEOd2Prm3x8zO6u67aA",
+            client_id: "tenant2-client-id-1"
+        }
+
+        try {
+            await axios.post(url, payload, {headers, timeout: 30000});
+        } catch (error) {
+            console.log("Received response:", error);
+            if (axios.isAxiosError(error) && error.response){
+                expect(error.response.status).toBe(401);
+            }
+        }
+    }, 50000)
+});
